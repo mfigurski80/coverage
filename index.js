26c66706605686576093145939b7dcc886a401fa
Mikolaj Figurski <miko.f80@gmail.com>
2026-03-24 17:23:22 -0400

    feat: impl index

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hello World");
+const core = require('@actions/core');
+const github = require('@actions/github');
+const path = require('path');
+const { parseCoverage } = require('./src/parser.js');
+const pushMetrics = require('./src/pusher.js');
+
+async function run() {
+  try {
+    const prometheusEndpoint = core.getInput('prometheus_endpoint', { required: true });
+    const labelsStr = core.getInput('labels');
+    const coverageFilePath = path.join(process.env.GITHUB_WORKSPACE, 'coverage.txt');
+
+    const coverageData = parseCoverage(coverageFilePath);
+
+    const labels = {};
+    if (labelsStr) {
+      for (const label of labelsStr.split(',')) {
+        const [key, value] = label.split('=');
+        labels[key.trim()] = value.trim();
+      }
+    }
+
+    const jobName = github.context.job;
+
+    await pushMetrics(prometheusEndpoint, jobName, coverageData, labels);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
